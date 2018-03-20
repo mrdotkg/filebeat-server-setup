70fb053a125bd6b474b3a1275dd758965d9e5a42
Kumar Gaurav <grv.rkg@gmail.com>
2018-03-21 04:26:39 +0530

    echo

--- filebeat_setup.sh
+++ filebeat_setup.sh
@@ -1,18 +1,22 @@
-#install
+#!/bin/bash  
+
+echo "**** downloading filebeat ****"
 curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-6.2.2-amd64.deb
+
+echo "**** installing filebeat ****"
 sudo dpkg -i filebeat-6.2.2-amd64.deb
 
-#run
+echo "**** running filebeat ****"
 sudo wget -O /etc/filebeat/filebeat.yml https://raw.githubusercontent.com/measdot/data/master/filebeat.yml 
 sudo filebeat -e setup --dashboards
 sudo service filebeat restart
 
-#enable
+echo "**** enabling filebeat mysql and apache2 modules ****"
 sudo filebeat -e modules enable apache2
 sudo filebeat -e modules enable mysql
 sudo filebeat setup -e
 
-#download
+echo "**** downloading sample mysql and apache2 logs ****"
 sudo mkdir /var/log/apache2
 sudo mkdir /var/log/mysql
 sudo wget -O /var/log/apache2/access.log https://raw.githubusercontent.com/measdot/data/master/access.log
@@ -20,5 +24,5 @@ sudo wget -O /var/log/apache2/error.log https://raw.githubusercontent.com/measdo
 sudo wget -O /var/log/mysql/error.log https://raw.githubusercontent.com/measdot/data/master/mysql/error.log
 sudo wget -O /var/log/mysql/error.log https://raw.githubusercontent.com/measdot/data/master/mysql/mysql-slow.log
 
-#feed
+echo "**** feeding logs to elk ****"
 sudo filebeat -e
